955870b25eb8cc39ffdf8bcf393e865d958bdd47
HollisDayCheer <hollisj@carleton.edu>
2015-12-14 09:06:42 -0800

    finally on git

---
+++ js/graphController.js
@@ -0,0 +1,156 @@
+angular.module('WikiGraph')
+  .controller("graphController", [ '$scope', '$http', function( $scope, $http) {
+    console.log('controller is working');
+    var ZoomChartsLicense = "ZCS-r50128xet: Free ZoomCharts development licence for hol..@..n.edu (valid for development and QA only)";
+var ZoomChartsLicenseKey = "bb7924e4f05f54b7fc036c803df2f441449f427f12ccc64ced"+
+"79fb9dcb2f6eb53fa80894138275980ccbc14bbc3376e5dfbe0e86ca89a703e9bdf49828b5f78"+
+"32c79df9f9222b625b90b676806d8a17a6c7b5e7207dbee9bfa320bf688c6666155e7cd78e40b"+
+"d5588523ad5247fef7866601a5274cabcb13ba5e3a73c0eb82223132f27400417c84398853cd3"+
+"885c4bbb3e1fd2116003c4f9c3202fee68771a3ab965bb3132780fb87bd95e86994a340650526"+
+"1fcdf53f933996ce1d010bfafdfaa56aeb4744437bb753c9847fc032b74294eb1540ad8faee50"+
+"a72fe6dd3a72417c044c83151f2e0bc88cedd4482f104a9a003e3f18ed42f8347acaa47499f77";
+    $scope.searchTerm = ""; //will be hit with an ng-model searchTerm
+    $scope.entriesInGraph = []; //array starts out empty, refreshes with each page refresh because 
+    //because the graph restarts, and we fill it as we add to our chart
+    // $scope.search = function(target){
+    //   // finds the closest wikipedia article (first one queried with an api call)
+    // }
+    $scope.pageLinks =[];
+    var numbNodes = 0;
+    var ourChart;
+    var graphStart = function(){
+        ourChart = new NetChart({
+            container: document.getElementById("chart-container"),
+            area: { height: 350 },
+            data: {  },
+            events: {
+                onDoubleClick: nodeClick
+            },
+            style: {
+                link: { fillColor: "limegreen" },
+                node: { imageCropping: true },
+                nodeStyleFunction: getImage
+            },
+            interaction: {
+                resizing: {
+                    enabled: false
+                },
+                zooming:{
+                    autoZoom: false,
+                    autoZoomAfterClick: false,
+                    autoZoomPositionEllasticity: false,
+                    zoomInOnDoubleClick: false
+                }
+            }
+
+
+         });
+    }
+
+    function getImage(node){
+        $http.jsonp("https://en.wikipedia.org/w/api.php?action=query&titles=" + node.id + "&prop=pageimages&format=json&pithumbsize=200&callback=JSON_CALLBACK").then(function(imageData){
+            console.log(Object.keys(imageData.data.query.pages)[0]);
+            if(imageData.data.query.pages[Object.keys(imageData.data.query.pages)[0]].thumbnail){
+                node.image = imageData.data.query.pages[Object.keys(imageData.data.query.pages)[0]].thumbnail.source;
+            }
+        })
+    }
+     function nodeClick(event){
+        if(event.clickNode){
+            $scope.continueGraph(event.clickNode.id);
+        }
+     }
+    //This is technically repeating myself, but I am ok with that because I want the set page to be a scoped variable, meaning it needs to 
+    //be a different function than the generic add nodes function
+     $scope.startGraph = function(title){
+        graphStart()
+        $http.jsonp("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=true&titles="+ title +"&format=json&callback=JSON_CALLBACK&redirects&").then(function(data){
+            $scope.ourID = Object.keys(data.data.query.pages)[0];
+            $scope.ourPage = (data.data.query.pages[$scope.ourID].extract);
+            // console.log($scope.ourPage);
+        }).then(function(data){
+            $scope.continueGraph(title);
+        });
+    }
+    $scope.continueGraph = function(title){
+        $http.jsonp("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=true&titles="+ title +"&format=json&callback=JSON_CALLBACK&redirects").then(function(data){
+            basePageID = Object.keys(data.data.query.pages)[0];
+            basePage = (data.data.query.pages[basePageID].extract.toLowerCase());
+            baseNodeID = data.data.query.pages[basePageID].title;
+            // console.log(basePage);
+        }).then(function(data){
+            console.log("first chart is");
+            console.log(ourChart.nodes());
+            var newLinks = getLinks(basePageID)               
+            $scope.pageLinks.concat(newLinks)
+            });
+        console.log(ourChart);
+        ourChart.updateStyle();
+         // ourChart.reloadData();
+      
+
+        var getLinks = function(pageID){
+            var pageLinks = [];
+
+            $http.jsonp("https://en.wikipedia.org/w/api.php?action=query&prop=links&format=json&plnamespace=0&indexpageids=&pageids="+ pageID + "&pllimit=500&format=json&callback=JSON_CALLBACK").then(function(data){
+                ourLinks = data.data.query.pages[pageID].links;
+                for(var i = 0; i < ourLinks.length; i++){
+                                // console.log(ourLinks[i]);
+                    if (basePage.indexOf(ourLinks[i].title.toLowerCase()) != -1){
+                        // console.log(ourLinks[i].title)
+                        pageLinks.push(ourLinks[i].title);
+                        var linkData = {nodes: [{id: ourLinks[i].title, loaded: true, style: {label: ourLinks[i].title, fillColor:  "#67B486"}}],
+                                       links: [{ id: ourLinks[i].title + "link", from: baseNodeID, to: ourLinks[i].title}]}; 
+                        ourChart.addData(linkData);
+                        numbNodes += 1;
+                        }
+                }
+                if(data.data.continue){
+                    getContinueLinks(pageID, data.data.continue.plcontinue);
+                }
+        });
+        function getContinueLinks(pageID, nextContinue){
+            var searchString = "https://en.wikipedia.org/w/api.php?action=query&prop=links&format=json&plnamespace=0&indexpageids=&pageids="+ pageID +"&pllimit=500&plcontinue="+ nextContinue + "&format=json&callback=JSON_CALLBACK";
+            $http.jsonp(searchString).success(function(data){
+                // console.log(data);
+                ourLinks = data.query.pages[basePageID].links;
+                for(var i = 0; i < ourLinks.length; i++){
+                        // console.log(ourLinks[i].title)
+                            // console.log(ourLinks[i]);
+                    if (basePage.indexOf(ourLinks[i].title.toLowerCase()) != -1){
+                        pageLinks.push(ourLinks[i].title);
+                        var newNode = {id: ourLinks[i].title, loaded: true, style: {label: ourLinks[i].title, fillColor:  "#67B486" }}
+                         var linkData = {nodes: [newNode],
+                                   links: [{ id: ourLinks[i].title + "link", from: baseNodeID, to: ourLinks[i].title}]};
+                        ourChart.addData(linkData);
+                        setInterval(function(){}, 100)
+                        numbNodes += 1;
+                    }
+                }
+                 if(data.continue){
+                     getContinueLinks(pageID, data.continue.plcontinue);
+                }
+             });
+        }
+        return pageLinks;
+    }
+     };
+
+
+
+
+      //recursive function getAllImages(pageID){
+        //apiPageID
+        //push those into the array
+        //if page.data.continue
+        //since inside of the function I won't need to pass in the array
+        //function getMOREpages(continue.plcontinue)
+        //api call with new continue
+        //push stuff in
+        //if page.continue
+        //getMOREpages(new.data.continue.plcontinue)
+      //}
+    
+    $scope.startGraph('Giant Panda');
+
+}]);
