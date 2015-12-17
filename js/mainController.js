fc4d00372a0850398ae806d9a7f2a75907e84328
HollisDayCheer <hollisj@carleton.edu>
2015-12-16 23:10:23 -0800

    checking url pages

--- js/mainController.js
+++ js/mainController.js
@@ -5,14 +5,37 @@ angular.module('WikiGraph')
 
   	$scope.findSpecific = function(search){
   		var goodPrefix = search.replace(/ /g,'');
-  		$http.get("http://" + goodPrefix + ".wikia.com").success(function(){
+  		if(checkUrl("http://" + goodPrefix + ".wikia.com"){
   			$scope.ourWiki = goodPrefix;
+  			$scope.wikiaSearch = "";
   			return true;
-  		}).error(function(){
-			$scope.wikiaSearch = "";
-			return false;
-		});
+  		} else{
+  			$scope.wikiaSearch = "";
+  			return false;
+  		}
+  // 		$http.get("http://" + goodPrefix + ".wikia.com").success(function(){
+  // 			$scope.ourWiki = goodPrefix;
+  // 			return true;
+  // 		}).error(function(){
+		// 	$scope.wikiaSearch = "";
+		// 	return false;
+		// });
   	}
+
+  	function checkUrl(url) {
+        var request = false;
+        if (window.XMLHttpRequest) {
+                request = new XMLHttpRequest;
+        } else if (window.ActiveXObject) {
+                request = new ActiveXObject("Microsoft.XMLHttp");
+        }
+
+        if (request) {
+                request.open("GET", url);
+                if (request.status == 200) { return true; }
+        }
+
+        
 
 
 
