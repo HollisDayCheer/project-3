c2c7dbc7b0c3a1e818a7cdc86acf614ee6bb2a75
HollisDayCheer <hollisj@carleton.edu>
2015-12-17 00:09:08 -0800

    fixed content string in adding html

--- js/specificWikiController.js
+++ js/specificWikiController.js
@@ -65,7 +65,7 @@ var ZoomChartsLicenseKey = "bb7924e4f05f54b7fc036c803df2f441449f427f12ccc64ced"+
                     {
                         contentString += "<p>" + paragraphs[i].innerHTML + "</p>";
                     }
-                    myEl.html("<div class = 'wiki-title' id='title-container'><h1>" + event.clickNode.id + "</h1></div><div class = 'wiki-content' id='content-container'><img src = '" + event.clickNode.image + "'><p>" + data.data.parse.text['*'] + "</p></div>");
+                    myEl.html("<div class = 'wiki-title' id='title-container'><h1>" + event.clickNode.id + "</h1></div><div class = 'wiki-content' id='content-container'><img src = '" + event.clickNode.image + "'><p>" + contentString + "</p></div>");
                 });
             } else{
                 $http.jsonp("http://"+ $scope.ourWiki + ".wikia.com/api.php?action=parse&section=0&page="+event.clickNode.id+"&format=json&callback=JSON_CALLBACK&redirects&prop=text").then(function(data){
